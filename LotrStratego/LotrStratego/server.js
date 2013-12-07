d5789a26336b781a23d19d94228b9cf2958f496c
Tirinst <MattEne@hotmail.co.uk>
2013-12-07 19:23:49 +0000

    Added random shit to Soln

--- LotrStratego/LotrStratego/server.js
+++ LotrStratego/LotrStratego/server.js
@@ -33,6 +33,7 @@ server.listen(port);
 console.log('http server listening on %d', port);
 
 var wss = new WebSocketServer({server: server});
+
 console.log('websocket server created');
 wss.on('connection', function(ws) {
     var id = setInterval(function() {
@@ -49,7 +50,6 @@ wss.on('connection', function(ws) {
     });
 
     console.log('websocket connection open');
-
     ws.on('close', function() {
         console.log('websocket connection close');
         clearInterval(id);
