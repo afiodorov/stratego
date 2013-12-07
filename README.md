1749b3b57f5ae760743f84c35e641ab9bac5d526
Tirinst <MattEne@hotmail.co.uk>
2013-12-07 22:44:57 +0000

    Tests

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
-# Node.js Websocket Test
+﻿# LotrStratego
 
-A tiny demo using the [einaros/ws](http://einaros.github.io/ws/) WebSockets implementation.
 
-# Running Locally
-
-``` bash
-npm install
-foreman start
-```
-
-# Running on Heroku
-
-``` bash
-heroku create
-heroku labs:enable websockets
-git push heroku master
-heroku open
-```
--- server.js
+++ server.js
@@ -1,57 +1,6 @@
-var WebSocketServer = require('ws').Server
-  , http = require('http')
-  , express = require('express')
-  , app = express()
-  , port = process.env.PORT || 5000
-  , MyString = require('./models/String.js');
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(express.bodyParser());
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-app.use(express.static(__dirname + '/public/'));
-
-app.post('/push', function(req, res) {
-  var string = req.body.string;
-  MyString.addString(string, function(err) {
-    if (err) {
-    	    console.log("db error");
-    	    throw err;
-    }
-    res.redirect('/');
-  });
-});
-
-var server = http.createServer(app);
-server.listen(port);
-
-console.log('http server listening on %d', port);
-
-var wss = new WebSocketServer({server: server});
-
-console.log('websocket server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function(){});
-    }, 1000);
-
-    MyString.find(function(err, strings){
-    	if(err) {throw err;}
-    	if(strings) {
-    	  strings.forEach(function(string) {
-	    ws.send(JSON.stringify(string.string));
-    	  });
-	}
-    });
-
-    console.log('websocket connection open');
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
-    });
-});
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
