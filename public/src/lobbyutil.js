5bebe542f27357f8ae2fc75b72be9176a9a59415
Tom Artiom Fiodorov <art.fiodorov@gmail.com>
2013-12-16 20:35:30 +0000

    preliminary split of code into modules

--- public/src/lobbyutil.js
+++ public/src/lobbyutil.js
@@ -3,84 +3,84 @@
 var socket = io.connect(location.origin);
 
 function startGame(pass) {
-    socket.emit('startGame', {pass: pass});
+  socket.emit('startGame', {pass: pass});
 }
 
 function getListOfGames(){
-    socket.emit('getListOfGames');
+  socket.emit('getListOfGames');
 }
 
 function setPlayerName(name) {
-    socket.emit('setPlayerName', {playerName : name});
+  socket.emit('setPlayerName', {playerName : name});
 }
 
 function requestGame(playerName) {
-    socket.emit('requestGame', {playerName: playerName});
+  socket.emit('requestGame', {playerName: playerName});
 }
 
 function acceptGame(playerName) {
-    socket.emit('acceptGame', {playerName: playerName});
+  socket.emit('acceptGame', {playerName: playerName});
 }
 
 socket.on('listOfGames', function(data) {
-	$("#gamesList").empty();
-	data.forEach(function(entry) {
-		$("#gamesList").append('<li>' + entry + '</li>');
-	});
+  $("#gamesList").empty();
+  data.forEach(function(entry) {
+    $("#gamesList").append('<li>' + entry + '</li>');
+  });
 });
 
 socket.on('gameStarted', function(data) {
-	$.pnotify({
-		title: 'New Game started',
-		text: encodeURI(data.playerName) + ' started a game with you!'
-	});
+  $.pnotify({
+    title: 'New Game started',
+    text: encodeURI(data.playerName) + ' started a game with you!'
+  });
 });
 
 socket.on('listOfPlayers', function(data) {
-	$("#playersList").empty();
-	data.forEach(function(entry) {
-		$("#playersList").append('<li>' + entry + '</li>');
-	});
+  $("#playersList").empty();
+  data.forEach(function(entry) {
+    $("#playersList").append('<li>' + entry + '</li>');
+  });
 });
 
 socket.on('requestGame', function(data) {
-	$.pnotify({
-		title: 'Game Request',
-		text: encodeURI(data.playerName) + ' requested a game. ' +
-		'<a href="#" id="acc' + encodeURI(data.playerName) + '">Accept</a>.'
-	});
-	$("a[id=acc" + encodeURI(data.playerName) + "]").click(function(){
-	acceptGame(data.playerName);
-	return false;});
+  $.pnotify({
+    title: 'Game Request',
+    text: encodeURI(data.playerName) + ' requested a game. ' +
+    '<a href="#" id="acc' + encodeURI(data.playerName) + '">Accept</a>.'
+  });
+  $("a[id=acc" + encodeURI(data.playerName) + "]").click(function(){
+  acceptGame(data.playerName);
+  return false;});
 
 });
 
 socket.on('addNewPlayer', function(data) {
-	if(data.isSelf === false) {
-		$("#playersList").append('<li id="p' + encodeURI(data.playerName) + '">' +
-			encodeURI(data.playerName) + '&nbsp;<a href="#" id="req' +
-			encodeURI(data.playerName) + '">Request</a></li>');
-		$("a[id=req" + encodeURI(data.playerName) + "]").click(function(){
-		requestGame(data.playerName);
-		return false;});
-	} else {
-		$("#playersList").append('<li id="___self">' +
-			encodeURI(data.playerName) + ' (You)</li>');
-		$("#playerName").val(encodeURI(data.playerName));
-		$("#__playerName").val(encodeURI(data.playerName));
-	}
+  if(data.isSelf === false) {
+    $("#playersList").append('<li id="p' + encodeURI(data.playerName) + '">' +
+      encodeURI(data.playerName) + '&nbsp;<a href="#" id="req' +
+      encodeURI(data.playerName) + '">Request</a></li>');
+    $("a[id=req" + encodeURI(data.playerName) + "]").click(function(){
+    requestGame(data.playerName);
+    return false;});
+  } else {
+    $("#playersList").append('<li id="___self">' +
+      encodeURI(data.playerName) + ' (You)</li>');
+    $("#playerName").val(encodeURI(data.playerName));
+    $("#__playerName").val(encodeURI(data.playerName));
+  }
 });
 
 socket.on('removePlayerName', function(data) {
-	if(data.isSelf === false) {
-		$('#p' + encodeURI(data.playerName)).remove();
-	} else {
-		$('#___self').remove();
-	}
+  if(data.isSelf === false) {
+    $('#p' + encodeURI(data.playerName)).remove();
+  } else {
+    $('#___self').remove();
+  }
 });
 
 socket.on('fChangedPlayerName', function() {
-	$('#playerNameErr').text("Such user already exists");	
+  $('#playerNameErr').text("Such user already exists"); 
 });
 
 $(function() {
--- server.js
+++ server.js
@@ -6,18 +6,14 @@ var jade = require('jade')
   , app = express()
   , port = process.env.PORT || 5000
   , cookieParser = express.cookieParser(secret)
-  , Game = require('./models/Game.js')
-  , MongoStore = require('connect-mongo')(express)
   , db = require('./lib/db.js')
-  , mongoStore = new MongoStore({url: db.url, auto_reconnect: true})
   , arr = require('./public/src/arr.js');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.bodyParser());
-app.use(express.cookieParser(''));
-app.use(express.session(
-  {store: new MongoStore({url: db.url}), secret: secret})); 
+app.use(cookieParser);
+app.use(express.session({store: db.mongoStore, secret: secret})); 
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -40,107 +36,11 @@ var server = http.createServer(app);
 server.listen(port);
 var io = require('socket.io').listen(server)
   , SessionSockets = require('session.socket.io')
-  , sessionSockets = new SessionSockets(io, mongoStore, cookieParser);
+  , sessionSockets = new SessionSockets(io, db.mongoStore, cookieParser);
 io.set('log level', 1);
-
 console.log('http server listening on %d', port);
 
-var clients = Object.create(null);
+var lobby = require('./lib/lobby.js');
+lobby.setio(io);
 
-var lobbyutils = require('./lib/lobbyutils.js');
-
-sessionSockets.on('connection', function (err, socket, session) {
-  socket.sid = session.id;
-
-  var onlineClients = io.sockets.clients();
-  var isDuplicate = false;
-  onlineClients.forEach(function(client) {
-    if ((client.id !== socket.id) && client.sid === socket.sid) {
-      // TODO handle this gracefully
-      // break the loop as well for efficiency!
-      isDuplicate = true;
-      console.log("duplicate client");
-    }
-	});
-
-  if(!isDuplicate) {
-  if((session.playerName === undefined) ||
-        // check for duplicate name in the db of sessions
-        ((session.playerName in clients) 
-       && (socket.sid !== clients[session.playerName].sid))) {
-    session.playerName = lobbyutils.genUniquePlayerName(clients);
-    session.save();
-  }
-  
-  clients[session.playerName] = {socket: socket, sid: socket.sid};
-  socket.broadcast.emit('addNewPlayer', {playerName: session.playerName,
-    isSelf: false});
-
-  onlineClients.forEach(function(client) {
-    var isSelf = (client.sid === socket.sid);
-    mongoStore.get(client.sid, function(err, session) {
-      socket.emit('addNewPlayer', {playerName:
-      session.playerName, isSelf: isSelf});
-    });
-  });
-
-  socket.on('setPlayerName', function(data) {
-    if(data.playerName in clients) {
-      socket.emit('fChangedPlayerName');  
-    } else {
-      delete clients[session.playerName];
-      socket.broadcast.emit('removePlayerName', {playerName:
-        session.playerName, isSelf: false});
-      socket.emit('removePlayerName', {playerName:
-        session.playerName, isSelf: true});
-
-      session.playerName = data.playerName;
-      session.save();
-      clients[session.playerName] = {socket: socket, sid: socket.sid};
-      socket.broadcast.emit('addNewPlayer', {playerName: session.playerName, 
-          isSelf: false});
-      socket.emit('addNewPlayer', {playerName:
-        session.playerName, isSelf: true});
-    }
-  });
-
-  socket.on('requestGame', function(data) {
-    try {  
-      var opponent = clients[data.playerName];
-      opponent.socket.emit('requestGame', {playerName:
-        session.playerName});
-      if (session.invites === undefined) {
-        session.invites = [];
-      }
-      // I am inviting the opponent to the game
-      session.invites.pushIfNotExist(opponent.sid);
-      session.save();
-    } catch(e) {
-      console.log(e);
-    }
-  });
-  
-  socket.on('acceptGame', function(data){
-    var opponent = clients[data.playerName];
-    mongoStore.get(opponent.sid, function (err, opsession) {
-                  if (opsession.invites.indexOf(socket.sid) !== -1) {
-      // I was invited by the opponent to this game!
-      Game.addPlayers(opponent.sid, socket.sid, function(err, game) {
-        opponent.socket.join(game.id);
-        socket.join(game.id);
-        // io.sockets.in(game.id).emit('gameJoined');
-        socket.emit('gameStarted', {playerName: opsession.playerName});
-        opponent.socket.emit('gameStarted',
-          {playerName: session.playerName});
-      });
-     }
-         });
-  });
-
-  socket.on('disconnect', function(socket) {
-    io.sockets.emit('removePlayerName', {playerName:
-      session.playerName, isSelf: false});
-  });
-
-  }
-});
+sessionSockets.on('connection', lobby.main);
