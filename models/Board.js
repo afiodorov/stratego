f0089364afbca83c4dee84c90c8bb10027ade358
Matthew David <MattEne@hotmail.co.uk>
2014-02-01 00:56:06 +0000

    Structure of board class

---
+++ models/Board.js
@@ -0,0 +1,23 @@
+﻿function board(){
+    this.pieces = [
+        function gandalf(){
+            this.name = "Gandalf";
+            },
+        function Sauron(){
+            this.name = "Sauron";
+            }];
+    this.squares = {
+        mordor: function (){
+            this.name = "mordor";
+        },
+        shire: function(){
+            this.name = "The Shire";
+        }
+    };
+    this.validMoves = [
+        {from: this.squares.mordor, to: this.squares.shire }
+        ];
+    this.validRetreats = [
+        {from: this.squares.shire, to: this.squares.mordor }
+        ];
+    }
