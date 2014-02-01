f150205c59a68f882b977194fe735428aa8090c4
Matthew David <MattEne@hotmail.co.uk>
2014-02-01 02:17:59 +0000

    Possibly less shit base

---
+++ models/Board/Pieces.js
@@ -0,0 +1,8 @@
+﻿var pieces = {
+        gandalf: function(){
+            this.name = "Gandalf";
+            },
+        sauron: function(){
+            this.name = "Sauron";
+            }
+        };
---
+++ models/Board/Squares.js
@@ -0,0 +1,12 @@
+﻿var squares = {
+        mordor: {
+            name: "Mordor",
+            draw: function(context){
+                }
+        },
+        shire: {
+            name: "The Shire",
+            draw: function(context){
+        }
+    }
+};
