21062d4a8e5da527661a9db3af9f5f3b34a5c298
Matthew David <MattEne@hotmail.co.uk>
2014-02-01 23:31:07 +0000

    Some playing around with objects

--- models/Board/Pieces.js
+++ models/Board/Pieces.js
@@ -1,8 +1,14 @@
-﻿var pieces = {
+﻿function getPieces(){
+    return {
         gandalf: function(){
             this.name = "Gandalf";
-            },
+        },
         sauron: function(){
             this.name = "Sauron";
-            }
-        };
+        }
+    };
+}
+function drawPiece(context, piece, position){
+    bacon ="";
+}
+
