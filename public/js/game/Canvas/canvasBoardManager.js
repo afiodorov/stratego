5418cd6d652b51ac3d835cfda5292a1ee7b77f48
Matthew David <MattEne@hotmail.co.uk>
2014-02-09 21:22:45 +0000

    Delete obsolete canvas

--- public/js/game/Canvas/canvasBoardManager.js
+++ public/js/game/Canvas/canvasBoardManager.js
@@ -1,57 +1,61 @@
 ﻿//var Kinetic = require('../../../vendor/kinetic-v5.0.1.min.js');
 
-var BOARD_WIDTH = 600;
-var BOARD_HEIGHT = 600;
+//var BOARD_WIDTH = 600;
+//var BOARD_HEIGHT = 600;
 
-// Animation.....................................................
-function draw() {
-    //var stage = new Kinetic.Stage({ container: "canvasPlayground", width: BOARD_WIDTH, height: BOARD_HEIGHT });
-    //var layer = new Kinetic.Layer();
-    //var hex = new Kinetic.RegularPolygon({
-    //    x: 100,
-    //    y: stage.height() / 2,
-    //    sides: 6,
-    //    radius: 70,
-    //    fill: '#00D2FF',
-    //    stroke: 'black',
-    //    strokeWidth: 4,
-    //    draggable: true
-    //});
+//// Animation.....................................................
+//function draw() {
+//    var stage = new Kinetic.Stage({ container: "canvasPlayground", width: BOARD_WIDTH, height: BOARD_HEIGHT });
+//    var layer = new Kinetic.Layer();
+//    var hex = new Kinetic.RegularPolygon({
+//        x: 100,
+//        y: stage.height() / 2,
+//        sides: 6,
+//        radius: 70,
+//        fill: '#00D2FF',
+//        stroke: 'black',
+//        strokeWidth: 4,
+//        draggable: true
+//    });
 
-    //var tiles = require("../structs/tiles.js");
+//    var tiles = require("../structs/tiles.js");
 
-    //var tileRects = new Array();
+//    var tileRects =[];
 
-   ////The game will be played top to bottom. "rows" are horizontal, and columns are verticle.
-   // for (var row = 1; row <= tiles.NUM_OF_ROWS; row++) {
-   //     var numCols = tiles.columnLimit(row);
-   //     for (var col = 1; col <= numCols ; col++) {
-   //         var tileWidth = BOARD_WIDTH / numCols;
-   //         var tileHeight = BOARD_HEIGHT / tiles.NUM_OF_ROWS;
-   //         //tileRects.push(new Kinetic.Rect({
-   //         //    width: tileWidth,
-   //         //    heigth: tileHeight,
-   //         //    x: tileWidth * (col - 1),//1 indexing ftw
-   //         //    y: tileHeight * (row -1),
-   //         //    fill: '#00D2FF'
-   //         //}));
-   //         tileRects.push(new Kinetic.Rect({
-   //             width: tileWidth,
-   //             height: tileHeight,
-   //             x: tileWidth * (col - 1),//1 indexing ftw
-   //             y: tileHeight * (row - 1),
-   //             fill: 'green',
-   //             stroke: 'black',
-   //             strokeWidth: 4
-   //         }));
+//    //The game will be played top to bottom. "rows" are horizontal, and columns are verticle.
+//    for (var row = 1; row <= tiles.NUM_OF_ROWS; row++) {
+//        var numCols = tiles.columnLimit(row);
+//        for (var col = 1; col <= numCols ; col++) {
+//            var rectWidth = BOARD_WIDTH / numCols;
+//            var rectHeight = BOARD_HEIGHT / tiles.NUM_OF_ROWS;
+//            var rectX = rectWidth * (col - 1);
+//            var rectY = rectHeight * (row - 1);
+//            tileRects.push(new Kinetic.Rect({
+//                width: rectWidth,
+//                height: rectHeight,
+//                x: rectX,//1 indexing ftw
+//                y: rectY,
+//                fill: 'green',
+//                stroke: 'black',
+//                strokeWidth: 4
+//            }));
+//            var text = new Kinetic.Text({
+//                x: rectX,
+//                y: rectY,
+//                text: tiles[[row, col]].name,
+//                fontSize: 18,
+//                fontFamily: 'Calibri',
+//                fill: '#555',
+//                width: 380,
+//                padding: 20,
+//                align: 'center'
+//            });
+//        }
+//    }
+//    layer.add(hex);
+//    tileRects.forEach(function (tile) { layer.add(tile); })
+//    stage.add(layer);
+//}
 
-
-   //     }
-   // }
-    //layer.add(hex);
-    //tileRects.forEach(function (tile) { layer.add(tile); })
-    //stage.add(layer);
-}
-
-draw();
+//draw();
 
