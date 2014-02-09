eefcbb77d2741d9464a6c0efcdab2ed44dea28b7
Matthew David <MattEne@hotmail.co.uk>
2014-02-09 20:51:38 +0000

    please work now, I don't think all of these files are needed

---
+++ public/game/Canvas/canvasBoardManager.js
@@ -0,0 +1,62 @@
+﻿/// <reference path="../structs/tiles.js" />
+var BOARD_WIDTH = 600;
+var BOARD_HEIGHT = 600;
+
+// Animation.....................................................
+function draw() {
+    var stage = new Kinetic.Stage({ container: "canvasPlayground", width: BOARD_WIDTH, height: BOARD_HEIGHT });
+    var layer = new Kinetic.Layer();
+    var hex = new Kinetic.RegularPolygon({
+        x: 100,
+        y: stage.height() / 2,
+        sides: 6,
+        radius: 70,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+    });
+
+    //var tiles = require("./utils/gameStructs").tiles;//Will need to be made to work.
+    //var _ = require('../lib/underscore.js');
+
+    
+
+    var tileRects = new Array();
+
+    var numTileColumns = tiles.length
+    for (var w = 0; w < numTileColumns; w++) {
+        var numTileRows = tiles[w].length;
+        for (var h = 0; h < numTileRows; h++) {
+            var rectWidth = BOARD_WIDTH /numTileColumns;
+            var rectHeight = BOARD_HEIGHT / numTileRows;
+            tileRects[w * numTileColumns + h] = new Kinetic.Rect({
+                width: rectWidth,
+                heith: rectHeight,
+                x: rectWidth * w,
+                y: rectHeight * h,
+                fill: '#FF00FF'
+                })
+        }
+    }
+
+
+
+
+
+
+
+    layer.add(hex);
+    layer.add(tileRects);
+    stage.add(layer);
+    //context.clearRect(0,0,canvas.width,canvas.height);
+    //update();
+    //var board = getBoard(canvas);
+    //board.draw(context);
+
+    //context.fillStyle = 'cornflowerblue';
+    //context.fillText(calculateFps().toFixed() + ' fps', 45, 50);
+}
+
+draw();
+
---
+++ public/js/game/Canvas/canvasBoardManager.js
@@ -0,0 +1,59 @@
+﻿/// <reference path="../structs/tiles.js" />
+/// <reference path="../../../vendor/kinetic-v5.0.1.min.js" />
+var Kinetic = require('../../../vendor/kinetic-v5.0.1.min.js');
+
+var BOARD_WIDTH = 600;
+var BOARD_HEIGHT = 600;
+
+// Animation.....................................................
+function draw() {
+    var stage = new Kinetic.Stage({ container: "canvasPlayground", width: BOARD_WIDTH, height: BOARD_HEIGHT });
+    var layer = new Kinetic.Layer();
+    var hex = new Kinetic.RegularPolygon({
+        x: 100,
+        y: stage.height() / 2,
+        sides: 6,
+        radius: 70,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+    });
+
+    var tiles = require("../structs/tiles.js");
+
+    var tileRects = new Array();
+
+    //The game will be played top to bottom. "rows" are horizontal, and columns are verticle.
+    for (var row = 1; row <= tiles.NUM_OF_ROWS; row++) {
+        var numCols = tiles.columnLimit(row);
+        for (var col = 1; col <= numCols ; col++) {
+            var tileWidth = BOARD_WIDTH / numCols;
+            var tileHeight = BOARD_HEIGHT / tiles.NUM_OF_ROWS;
+            //tileRects.push(new Kinetic.Rect({
+            //    width: tileWidth,
+            //    heigth: tileHeight,
+            //    x: tileWidth * (col - 1),//1 indexing ftw
+            //    y: tileHeight * (row -1),
+            //    fill: '#00D2FF'
+            //}));
+            tileRects.push(new Kinetic.Rect({
+                width: tileWidth,
+                height: tileHeight,
+                x: tileWidth * (col - 1),//1 indexing ftw
+                y: tileHeight * (row - 1),
+                fill: 'green',
+                stroke: 'black',
+                strokeWidth: 4
+            }));
+
+
+        }
+    }
+    layer.add(hex);
+    tileRects.forEach(function (tile) { layer.add(tile); })
+    stage.add(layer);
+}
+
+draw();
+
