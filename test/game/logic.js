619f184aaf839c5cf09b481416fa8312d6768ab9
Tom Artiom Fiodorov <art.fiodorov@gmail.com>
2014-02-22 18:56:37 +0000

    added a logic test class

---
+++ test/game/logic.js
@@ -0,0 +1,11 @@
+/*global describe, it*/
+'use strict';
+var assert = require('assert');
+var _ = require('../../public/js/lib/underscore.js');
+var logic = require('../../public/js/game/logic.js');
+
+describe('GameLogic', function() {
+  describe('#generatePiecePositions', function() {
+    console.log(logic.generatePiecePositions());
+  });
+});
