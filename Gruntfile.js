19f6df4b8c4119cf2cfa02e6119e31d6b7fa642f
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2014-03-13 14:06:38 +0000

    Grunt now works on computers with less than 4 cores

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
       dev: {
         tasks: ['nodemon', 'node-inspector', 'browserify', 'watch'],
         options: {
+          limit: 4,
           logConcurrentOutput: true
         }
       }
