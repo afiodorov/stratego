ade02113b54bbd186f53e616b85f442186b4b8df
Matthew David <MattEne@hotmail.co.uk>
2014-04-13 01:48:37 +0100

    Got a piece being drawn!

--- public/js/game/Canvas/tileLayer.js
+++ public/js/game/Canvas/tileLayer.js
@@ -1,96 +1,88 @@
-﻿
+﻿/// <reference path="../structs/board.js" />
+
 var tileLayer = function (boardWidth, boardHeight) {
-    var MAXPIECESINROW = 6;
-    var MAXPIECESINCOL = 7;
+  var MAXPIECESINROW = 6;
+  var MAXPIECESINCOL = 7;
 
-    var Kinetic = require('../../../vendor/kinetic-v5.0.1.min.js');
-    var tiles = require("../structs/tiles.js");
-    var tileGroups = [];
+  var Kinetic = require('../../../vendor/kinetic-v5.0.1.min.js');
+  var boardMod = require("../structs/board.js");
+  var tileGroups = [];
 
-    function getTileGroup(rectWidth, rectHeight, rectX, rectY) {
-        var group = new Kinetic.Group({
-            width: rectWidth,
-            height: rectHeight,
-            x: rectX,
-            y: rectY
-        });
-        group.add(new Kinetic.Rect({
-            width: rectWidth,
-            height: rectHeight,
-            fill: 'green',
-            stroke: 'black',
-            strokeWidth: 4
-        }));
-        group.add(new Kinetic.Text({
-            width: rectWidth,
-            height: rectHeight,
-            text: tiles[[row, col]].name + " " + (row -1) + " " + (col -1),
-            fontSize: 18,
-            fontFamily: 'Calibri',
-            fill: '#555',
-            padding: 20,
-            align: 'center'
-        }));
-        return group;
-    }
+  var board = boardMod.makeBoard(boardMod.SIDE_DARK);
 
-    function getPieceGroup(piece) {
-        var radius = Math.min(boardWidth / (MAXPIECESINROW * 2) , boardHeight / (MAXPIECESINCOL * 2));
-        var group = new Kinetic.Group({
-            width: radius * 2,
-            height: radius * 2,
-        });
-        group.add(new Kinetic.Circle({
-            radius: radius,
-            x: radius,
-            y: radius,
-            fill: 'red',
-            stroke: 'black',
-            strokeWidth: 4
-        }));
-        group.add(new Kinetic.Text({
-            width: radius * 2,
-            height: radius * 2,
-            text: piece.name,
-            fontSize: 18,
-            fontFamily: 'Calibri',
-            fill: '#555',
-            padding: 20,
-            align: 'center'
-        }));
+  board.tiles[1][1].addPiece(board.lightPieces[0]);
+
+  function getTileGroup(tile, rectWidth, rectHeight, rectX, rectY) {
+    var group = new Kinetic.Group({
+      width: rectWidth,
+      height: rectHeight,
+      x: rectX,
+      y: rectY
+    });
+    group.add(new Kinetic.Rect({
+      width: rectWidth,
+      height: rectHeight,
+      fill: 'green',
+      stroke: 'black',
+      strokeWidth: 4
+    }));
+    group.add(new Kinetic.Text({
+      width: rectWidth,
+      height: rectHeight,
+      text: tile.name + " " + row + " " + col,
+      fontSize: 18,
+      fontFamily: 'Calibri',
+      fill: '#555',
+      padding: 20,
+      align: 'center'
+    }));
+
+    for (var i = 0; i < tile.getPieces().length; i++) {
+      group.add(getPieceGroup(tile.getPieces()[i]));
     }
-    
-    //The game will be played top to bottom. "rows" are horizontal, and columns are verticle.
-    for (var row = 1; row <= tiles.numRows ; row++) {
-        var numCols = tiles.numCols(row);
-        for (var col = 1; col <= numCols ; col++) {
-            var rectWidth = boardWidth / numCols;
-            var rectHeight = boardHeight / tiles.numRows;
-            var rectX = rectWidth * (col - 1);
-            var rectY = rectHeight * (row - 1);
-            tileGroups.push(
-                getTileGroup(rectWidth, rectHeight, rectX, rectY).add(
-                new Kinetic.Circle({
-                    radius: rectHeight / 4,
-                    fill: 'red',
-                    stroke: 'black',
-                    strokeWidth: 4
-                })).add(
-                new Kinetic.Text({
-                    width: rectHeight,
-                    height: rectHeight,
-                    text: tiles[[row, col]].name + " " + (row - 1) + " " + (col - 1),
-                    fontSize: 18,
-                    fontFamily: 'Calibri',
-                    fill: '#555',
-                    padding: 20,
-                    align: 'center'
-                })));
-        }
+    return group;
+  }
+
+  function getPieceGroup(piece) {
+    var radius = Math.min(boardWidth / (MAXPIECESINROW * 2), boardHeight / (MAXPIECESINCOL * 2));
+    var group = new Kinetic.Group({
+      width: radius * 2,
+      height: radius * 2,
+    });
+    group.add(new Kinetic.Circle({
+      radius: radius,
+      x: radius,
+      y: radius,
+      fill: 'red',
+      stroke: 'black',
+      strokeWidth: 4
+    }));
+    group.add(new Kinetic.Text({
+      width: radius * 2,
+      height: radius * 2,
+      text: piece.name,
+      fontSize: 18,
+      fontFamily: 'Calibri',
+      fill: '#555',
+      padding: 20,
+      align: 'center'
+    }));
+    return group;
+  }
+
+  //The game will be played top to bottom. "rows" are horizontal, and columns are verticle.
+  for (var row = 0; row < board.tiles.length ; row++) {
+    for (var col = 0; col < board.tiles[row].length ; col++) {
+      var rectWidth = boardWidth / board.tiles[row].length;
+      var rectHeight = boardHeight / board.tiles.length;
+      var rectX = rectWidth * col;
+      var rectY = rectHeight * row;
+      tileGroups.push(getTileGroup(board.tiles[row][col], rectWidth, rectHeight, rectX, rectY));
     }
-    var layer = new Kinetic.Layer();
-    tileGroups.forEach(function (tile) { layer.add(tile); })
-    return layer;
+  }
+  var layer = new Kinetic.Layer();
+  tileGroups.forEach(function (a) { layer.add(a); });
+  return layer;
 }
 
 module.exports = tileLayer;
--- public/js/game/structs/board.js
+++ public/js/game/structs/board.js
@@ -1,13 +1,14 @@
 ﻿/// <reference path="tile.js" />
 /// <reference path="../../lib/underscore.js" />
+/// <reference path="piece.js" />
 
 var SIDE_DARK = 4325897;
 var SIDE_LIGHT = 203423487;
 
 var makeBoard = function (side) {
- // We are defining the tiles' position by its' location in the array.
- // All postional relatives will be assuming that the Shire is South, and that Mordor is North.
- // The shire will be at the top of the board with a 0 row index, Mordor at the bottom, with the highest row index.
+  // We are defining the tiles' position by its' location in the array.
+  // All postional relatives will be assuming that the Shire is South, and that Mordor is North.
+  // The shire will be at the top of the board with a 0 row index, Mordor at the bottom, with the highest row index.
   var tiles = (function () {
     var makeTile = require('./tile.js');
     var _ = require("../../lib/underscore.js");
@@ -37,10 +38,38 @@ var makeBoard = function (side) {
     return tilesArr;
   })();
 
+  var allPieces = (function () {
+    var makePiece = require('./piece.js');
+    var pieces = [];
+    pieces.push(makePiece('gandalf', 'Dark Player must play his card first', 5, SIDE_LIGHT));
+    pieces.push(makePiece('aragorn', 'May attack any adjacent region', 4, SIDE_LIGHT));
+    pieces.push(makePiece('boromir', 'Both Boromir and Enemy character are instantly defeated', 0, SIDE_LIGHT));
+    pieces.push(makePiece('frodo', 'When attacked, may retreat sideways', 1, SIDE_LIGHT));
+    pieces.push(makePiece('gimly', 'Instantly defeats the Orcs', 3, SIDE_LIGHT));
+    pieces.push(makePiece('legolas', 'Instantly defeats the Flying Nazgul', 3, SIDE_LIGHT));
+    pieces.push(makePiece('merry', 'Instantly defeats the Witch King', 2, SIDE_LIGHT));
+    pieces.push(makePiece('pippin', 'When attacking may retreat backwards', 1, SIDE_LIGHT));
+    pieces.push(makePiece('sam', 'When with Frodo, is Strength 5 & may replace Frodo in battle', 2, SIDE_LIGHT));
+
+    pieces.push(makePiece('orcs', 'When attacking instantly defeats the first character', 2, SIDE_DARK));
+    pieces.push(makePiece('shelob', 'After Shelob defeats an Enemy character, she is immediately returned to Gondor', 5, SIDE_DARK));
+    pieces.push(makePiece('saruman', 'May decide that no cards are played', 4, SIDE_DARK));
+    pieces.push(makePiece('flying nazgul', 'May attack a single Light character anywhere on the board', 3, SIDE_DARK));
+    pieces.push(makePiece('barlog', 'When in Moria instantly defeats any Character using the Tunnel', 5, SIDE_DARK));
+    pieces.push(makePiece('warg', 'Enemy character\'s text is ignored', 2, SIDE_DARK));
+    pieces.push(makePiece('black rider', 'May move forward any number of regions to attack', 4, SIDE_DARK));
+    pieces.push(makePiece('witch king', 'May attack sideways', 5, SIDE_DARK));
+    pieces.push(makePiece('cave troll', 'The Dark Player\'s card has no value or effect', 9, SIDE_DARK));
+    return pieces;
+  })();
+
+  var lightPieces = allPieces.filter(function (p) { return p.side === SIDE_LIGHT });
+
+  var darkPieces = allPieces.filter(function (p) { return p.side === SIDE_DARK });
+
   var sideString = side === SIDE_DARK ? "dark" : "light";
-
-
-  var validMoveFuncs = (function(){
+  
+  var validMoveFuncs = (function () {
 
     var getVerticleAdjTiles = function (tile, goingNorth) {
       var targetRow = tile.position.y + (goingNorth ? -1 : +1);
@@ -55,7 +84,7 @@ var makeBoard = function (side) {
       }
     }
 
-    var isDefined = function(obj){
+    var isDefined = function (obj) {
       return obj !== undefined;
     }
 
@@ -68,7 +97,7 @@ var makeBoard = function (side) {
     }
 
     //potential refers to it being prior to the check that the square actually has space.
-    var getPotentialForwards = function(tile){
+    var getPotentialForwards = function (tile) {
       var forwardTiles = getVerticleAdjTiles(tile, side === SIDE_DARK);
       if (side === SIDE_LIGHT && tile === tiles[2][1]) {//You can take a shortcut from this square if you're on the light side. 
         forwardTiles.push(tiles[4][1]);
@@ -77,31 +106,31 @@ var makeBoard = function (side) {
     }
 
     var getPotentialBackwards = function (tile) {
-        return getVerticleAdjTiles(tile, side === SIDE_LIGHT);
+      return getVerticleAdjTiles(tile, side === SIDE_LIGHT);
     }
 
-    var getPotentialSideways = function(tile){
-        if (tile.position.y === 3) {//Mountains, maybe this should be stored in the tile itself?
+    var getPotentialSideways = function (tile) {
+      if (tile.position.y === 3) {//Mountains, maybe this should be stored in the tile itself?
+        return [];
+      }
+      if (tile.position.y === 4) {//River, see above.
+        if (side === SIDE_DARK) {
           return [];
         }
-        if (tile.position.y === 4) {//River, see above.
-          if (side === SIDE_DARK) {
-            return [];
-          }
-          return [tiles[4][tile.position.x -1]].filter(isDefined);
-        }
-        return getHorizontalAdjTiles(tile);
+        return [tiles[4][tile.position.x - 1]].filter(isDefined);
       }
+      return getHorizontalAdjTiles(tile);
+    }
 
-    var validSideways = function(tile){
+    var validSideways = function (tile) {
       return getPotentialSideways(tile).filter(isNotAtMaxCap);
     }
 
-    var validForwards = function(tile){
+    var validForwards = function (tile) {
       return getPotentialForwards(tile).filter(isNotAtMaxCap);
     }
 
-    var validBackwards = function(tile){
+    var validBackwards = function (tile) {
       return getPotentialBackwards(tile).filter(isNotAtMaxCap);
     }
 
@@ -113,16 +142,17 @@ var makeBoard = function (side) {
       sideways: validSideways
     };
   })();
- 
+
   return {
     tiles: tiles,
     sideString: sideString,
-    validMoveFuncs: validMoveFuncs
+    validMoveFuncs: validMoveFuncs,
+    lightPieces: lightPieces
   };
 }
 
 
-module.exports = 
+module.exports =
   {
     makeBoard: makeBoard,
     SIDE_DARK: SIDE_DARK,
