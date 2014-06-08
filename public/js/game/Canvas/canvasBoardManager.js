f676424ae990acc1ba86b578f30af4812bb0f3f1
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2014-06-08 12:58:36 +0100

    using Kinect from package.json

--- public/js/game/Canvas/canvasBoardManager.js
+++ public/js/game/Canvas/canvasBoardManager.js
@@ -1,14 +1,15 @@
-﻿/// <reference path="tileLayer.js" />
-var Kinetic = require('../../../vendor/kinetic-v5.0.1.min.js');
+﻿/*global window*/
+var Kinetic = require('kinetic');
 
 var BOARD_WIDTH = 600;
 var BOARD_HEIGHT = 600;
 
 // Animation.....................................................
 function draw(container) {
-  var stage = new Kinetic.Stage({ container: container, width: BOARD_WIDTH, height: BOARD_HEIGHT });
-  var tileLayer = require('./tileLayer.js')(BOARD_WIDTH, BOARD_HEIGHT)
-    
+  var stage = new Kinetic.Stage({ container: container, width: BOARD_WIDTH,
+    height: BOARD_HEIGHT });
+  var tileLayer = require('./tileLayer.js')(BOARD_WIDTH, BOARD_HEIGHT);
+
   var layer = new Kinetic.Layer();
 
   var circle = new Kinetic.Circle({
@@ -28,5 +29,6 @@ function draw(container) {
   //tileLayer.setZIndex(3);
 }
 
-window.draw = draw;
-module.exports = draw;
+//window.draw = draw;
+//draw();
+//module.exports = draw;
